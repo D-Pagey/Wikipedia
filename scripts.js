3f2f0ce2a4b8c99bd26ae828443210a99df22da8
D-Pagey <dan.page91@gmail.com>
2017-10-31 02:35:18 +0000

    Not much progress.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
+// Random article button
 function randomArticle() {
   const url = "https://en.wikipedia.org/wiki/Special:Random";
   window.open(url);
 }
 
 document.getElementById("random").addEventListener("click", randomArticle);
+
+// Fetch Search Results
+
+/* To Do:
+- get API call for SEARCH results
+- input users search results
+- return JSON data of users search results
+- learn how to create containers
+- iterate through return object and create containers for each
+- remove search bar and button, create back button
+- https://github.com/ksc23/wikipedia-viewer/blob/master/scripts.js
+*/
