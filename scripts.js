a53a79f00f706d6b45812ddf8acfbcab7160e5b7
D-Pagey <dan.page91@gmail.com>
2017-11-01 00:37:54 +0000

    Calls API on page load without user input.

--- scripts.js
+++ scripts.js
@@ -7,13 +7,29 @@ function randomArticle() {
 document.getElementById("random").addEventListener("click", randomArticle);
 
 // Fetch Search Results
+const apiUrl = "https://en.wikipedia.org/w/api.php?action=query&format=json&origin=*&list=search&srsearch=";
+const userInput = "ladders";
+
+function search() {
+fetch(apiUrl + userInput)
+  .then((resp) => resp.json())
+  .then(function(articles) {
+  console.log(articles);
+})
+
+  .catch(function(error) {
+    console.log("Something went wrong");
+})
+}
+
+search();
 
 /* To Do:
-- get API call for SEARCH results
+- Understand query string
 - input users search results
 - return JSON data of users search results
 - learn how to create containers
 - iterate through return object and create containers for each
-- remove search bar and button, create back button
+- remove search bar, random article and button, create back button
 - https://github.com/ksc23/wikipedia-viewer/blob/master/scripts.js
 */
